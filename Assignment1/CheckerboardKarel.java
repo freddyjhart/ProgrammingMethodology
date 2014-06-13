10fba4b5a4979920dce7f0e0014b6d1864f77e46
Freddy Hart <freddy@Freds-MacBook-Pro.local>
2014-06-13 12:19:45 -0400

    Initial checkin of ProgrammingMethodology exercises

---
+++ Assignment1/CheckerboardKarel.java
@@ -0,0 +1,42 @@
+/*
+
+ * File: CheckerboardKarel.java
+ * ----------------------------
+ * When you finish writing it, the CheckerboardKarel class should draw
+ * a checkerboard using beepers, as described in Assignment 1.  You
+ * should make sure that your program works for all of the sample
+ * worlds supplied in the starter folder.
+ */
+
+import stanford.karel.*;
+
+public class CheckerboardKarel extends SuperKarel {
+
+	public void run()
+	{
+	
+		while (true) 
+		{
+			if (frontIsClear()) 
+			{
+				move();
+				putBeeper();
+				if(frontIsClear())
+				{
+					move();
+				}
+				else
+				{
+					turnLeft();
+					move();
+				}
+			}
+			else
+			{
+				turnLeft();
+				move();
+			}
+			
+		}
+	}
+}
